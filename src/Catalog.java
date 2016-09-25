956bf63aaea91702e507f1039562c596c03fb346
Daniel <danielstefanik36@gmail.com>
2016-09-25 21:04:34 +0200

    a new approach of class Catalog

--- src/Catalog.java
+++ src/Catalog.java
@@ -1,63 +1,54 @@
 import java.io.File;
-import java.io.IOException;
-
 
 public class Catalog {
 	private File[] catList;
 	private File[] fileList;
 	private String path;
 	
-	public Catalog(String _path) throws IOException{
-		
+	public Catalog(String _path) throws Exception{
+		changePath(_path);
+	}
+	
+	public void changePath(String _path) throws Exception{
 		path = _path;
 		//System.out.println(path);
 		File dir = new File(path);		
-		try{	
-			if( !dir.isDirectory() ){
-				throw new IOException("File should be directory.");	
+		if( !dir.isDirectory() ){
+			throw new Exception("File should be directory.");	
+		}
+		else{
+			int i=0;
+			int j=0;
+			for(File f : dir.listFiles()){
+				if(f.isDirectory())
+					i++;
+				else
+					j++;
 			}
-			else{
-				int i=0;
-				int j=0;
-				for(File f : dir.listFiles()){
-					if(f.isDirectory())
-						i++;
-					else
-						j++;
-				}
 				
-				if(i>0)
-					catList = new File[i];
-				if(j>0)
-					fileList = new File[j];
+			if(i>0)
+				catList = new File[i];
+			if(j>0)
+				fileList = new File[j];
 
-				//System.out.println("i: "+i+", j: "+j);
-				i=0;
-				j=0;
-				for(File f : dir.listFiles()){
-					if(f.isDirectory()){
-						catList[i] = f;
-						i++;
-					}
-					else{
-						fileList[j] = f;
-						j++;
-					}
+			//System.out.println("i: "+i+", j: "+j);
+			i=0;
+			j=0;
+			for(File f : dir.listFiles()){
+				if(f.isDirectory()){
+					catList[i] = f;
+					i++;
 				}
-				
-			}
-		}
-		catch(IOException e){
-			System.out.println(e);
-		}
-			
+				else{
+					fileList[j] = f;
+					j++;
+				}
+			}		
+		}			
 	}
 	
 	public void showFiles(){
-		if(fileList==null){
-			
-		}
-		else{
+		if(fileList!=null){
 			for(File file : fileList){
 	            System.out.println(file.getName());
 			}
@@ -65,11 +56,7 @@ public class Catalog {
 	}
 	
 	public void showCatalogs(){
-
-		if(catList==null){
-			
-		}
-		else{
+		if(catList!=null){
 			for(File cat : catList){
             	System.out.println("- "+cat.getName());
 			}
@@ -78,6 +65,42 @@ public class Catalog {
 	
 	public String getPath(){
 		return path;
+	}
+	public String getParentPath() throws Exception {
+		if(!this.hasParent()){
+			throw new Exception("Directory has not parent directory.");
+		}
+		
+		
+		String[] pathParts= path.split("/");
+		String parentPath=pathParts[0]+"/";
+		
+		for(int i=1;i<pathParts.length-1;i++){
+
+			if(i==1){
+				parentPath = parentPath + pathParts[i];
+			}
+			else {
+				parentPath = parentPath + "/" + pathParts[i];
+			}
+
+		}
+		//System.out.print(parentPath);
+		return parentPath;
+	}
+	public void changeToParent() throws Exception{
+		
+		changePath(this.getParentPath());
+		
+	}
+	public boolean hasParent(){
+		String[] pathParts = path.split("/");
+		if(pathParts.length == 1){
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
 	
 }
--- src/FileCommander.java
+++ src/FileCommander.java
@@ -1,11 +1,73 @@
 import java.io.IOException;
 import java.util.Scanner;
+
+
 public class FileCommander {
-	public static final String MAIN_PATH = "C:/Users/Daniel/Desktop/"; 
+
+	public static final String MAIN_PATH = "C:/Users/Daniel/Desktop"; 
+	//public static final String MAIN_PATH = "C:/"; 
 	
-	public static void main(String[] args) throws InterruptedException, IOException {
+	public static void main(String[] args) throws InterruptedException, Exception {
 		
-		String[] pathParts= MAIN_PATH.split("/");
+		Catalog dir = new Catalog(MAIN_PATH);
+		
+		dir.hasParent();
+		
+		Scanner readCommand = new Scanner(System.in);
+		System.out.println(dir.getPath());
+		dir.showCatalogs();
+		
+		while(readCommand.hasNextLine()){
+			
+			String command = readCommand.nextLine();
+			
+			if(command.substring(0, 1).equals(":")){
+				
+					
+			}
+			else{
+				
+				if(command.equals("..")){
+					try{
+						dir.changeToParent();
+					}
+					catch(Exception e){
+						System.out.println(e);
+						System.out.println(dir.getPath());
+						dir.showCatalogs();
+						continue;
+					}
+				}
+				else{
+					
+					try{
+						String newPath;
+						if(dir.hasParent()){
+							newPath = dir.getPath()+"/"+command;
+						}
+						else{
+							newPath = dir.getPath()+command;
+						}
+						
+						dir.changePath(newPath);
+					}
+					catch(IOException e){
+						System.out.println(e);
+						System.out.println(dir.getPath());
+						dir.showCatalogs();
+						continue;
+					}
+				}
+				
+				System.out.println(dir.getPath());
+				dir.showCatalogs();
+			}
+
+			//dir[depth].showFiles();
+		}
+		readCommand.close();
+		
+		/*String[] pathParts= MAIN_PATH.split("/");
 		Catalog[] dir = new Catalog[20]; 
 		pathParts[0]=pathParts[0]+"/";
 		int depth=0;
@@ -19,31 +81,51 @@ public class FileCommander {
 		}
 
 		Scanner readCommand = new Scanner(System.in);
+		System.out.println(dir[depth].getPath());
+		dir[depth].showCatalogs();
 		
 		while(readCommand.hasNextLine()){
-
-			System.out.println(dir[depth].getPath());
-			dir[depth].showCatalogs();
 			
 			String command = readCommand.nextLine();
-			//System.out.println(command);
-			if(command.equals("..")){
-				if(depth<0){
+			
+			if(command.substring(0, 1).equals(":")){
+				
 					
-				}
-				else{
-					depth--;
-					//System.out.println(depth);
-				}
 			}
 			else{
-				depth++;
-				dir[depth] = new Catalog(dir[depth-1].getPath()+"/"+command);
+				if(command.equals(".")){
+					if(depth>0){
+						depth--;
+						//System.out.println(depth);
+					}
+				}
+				if(command.equals("..")){
+					if(depth>0){
+						depth--;
+						//System.out.println(depth);
+					}
+				}
+				else{
+					
+					try{
+						dir[depth+1] = new Catalog(dir[depth].getPath()+command+"/");
+					}
+					catch(IOException e){
+						System.out.println(e);
+						System.out.println(dir[depth].getPath());
+						dir[depth].showCatalogs();
+						continue;
+					}
+					depth++;
+				}
+				
+				System.out.println(dir[depth].getPath());
+				dir[depth].showCatalogs();
 			}
 
 			//dir[depth].showFiles();
 		}
-		readCommand.close();
+		readCommand.close();*/
 	}
 
 }
