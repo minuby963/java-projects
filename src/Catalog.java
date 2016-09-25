896546d89542a842a2f75e3f255c33f460ed4196
Daniel <danielstefanik36@gmail.com>
2016-09-25 21:29:16 +0200

    some problems durning merging

--- src/Catalog.java
+++ src/Catalog.java
@@ -1,62 +1,73 @@
 import java.io.File;
-import java.io.IOException;
-
 
 public class Catalog {
-	File[] catList;
-	File[] fileList;
-	String path;
-	public Catalog(String _path) throws IOException{
-		
-		path = _path;
-		//System.out.println(path);
-		File dir = new File(path);		
-		try{	
-			if( !dir.isDirectory() ){
-				throw new IOException("File should be directory.");	
-			}
-			else{
-				int i=0;
-				int j=0;
-				for(File f : dir.listFiles()){
-					if(f.isDirectory())
-						i++;
-					else
-						j++;
-				}
-				
-				if(i>0)
-					catList = new File[i];
-				if(j>0)
-					fileList = new File[j];
-
-				//System.out.println("i: "+i+", j: "+j);
-				i=0;
-				j=0;
-				for(File f : dir.listFiles()){
-					if(f.isDirectory()){
-						catList[i] = f;
-						i++;
-					}
-					else{
-						fileList[j] = f;
-						j++;
-					}
-				}
-				
-			}
-		}
-		catch(IOException e){
-			System.out.println(e);
-		}
-			
-	}
+	private File[] catList;
+	private File[] fileList;
+	private String path;
 	
-	public void showFiles(){
-		if(fileList==null){
+	public Catalog(String _path) throws Exception{
+		changePath(_path);
+	}
+	public void checkAndChangeCatalog(String command) throws Exception{
+		if(command.equals("..")){
+			
+			changeToParent();
 			
 		}
 		else{
+			String newPath;
+			if(hasParent()){
+				newPath = getPath()+"/"+command;
+			}
+			else{
+				newPath = getPath()+command;
+			}
+				
+			changePath(newPath);
+			
+		}
+	}
+	
+	public void changePath(String _path) throws Exception{
+		path = _path;
+		//System.out.println(path);
+		File dir = new File(path);		
+		if( !dir.isDirectory() ){
+			throw new Exception("File should be directory.");	
+		}
+		else{
+			int i=0;
+			int j=0;
+			for(File f : dir.listFiles()){
+				if(f.isDirectory())
+					i++;
+				else
+					j++;
+			}
+				
+			if(i>0)
+				catList = new File[i];
+			if(j>0)
+				fileList = new File[j];
+
+			//System.out.println("i: "+i+", j: "+j);
+			i=0;
+			j=0;
+			for(File f : dir.listFiles()){
+				if(f.isDirectory()){
+					catList[i] = f;
+					i++;
+				}
+				else{
+					fileList[j] = f;
+					j++;
+				}
+			}		
+		}			
+	}
+	
+	public void showFiles(){
+		if(fileList!=null){
 			for(File file : fileList){
 	            System.out.println(file.getName());
 			}
@@ -64,11 +75,7 @@ public class Catalog {
 	}
 	
 	public void showCatalogs(){
-
-		if(catList==null){
-			
-		}
-		else{
+		if(catList!=null){
 			for(File cat : catList){
             	System.out.println("- "+cat.getName());
 			}
@@ -77,6 +84,42 @@ public class Catalog {
 	
 	public String getPath(){
 		return path;
+	}
+	public String getParentPath() throws Exception {
+		if(!this.hasParent()){
+			throw new Exception("Directory has not parent directory.");
+		}
+		
+		
+		String[] pathParts= path.split("/");
+		String parentPath=pathParts[0]+"/";
+		
+		for(int i=1;i<pathParts.length-1;i++){
+
+			if(i==1){
+				parentPath = parentPath + pathParts[i];
+			}
+			else {
+				parentPath = parentPath + "/" + pathParts[i];
+			}
+
+		}
+		//System.out.print(parentPath);
+		return parentPath;
+	}
+	public void changeToParent() throws Exception{
+		
+		changePath(this.getParentPath());
+		
+	}
+	public boolean hasParent(){
+		String[] pathParts = path.split("/");
+		if(pathParts.length == 1){
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
 	
 }
